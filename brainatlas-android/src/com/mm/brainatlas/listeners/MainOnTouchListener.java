e6280d639962406a905fa39a1a8e53c6c6dff9b2
Tjesto <Tjesto@bitbucket.org>
2014-05-04 14:01:50 +0200

    Minor changes

--- brainatlas-android/src/com/mm/brainatlas/listeners/MainOnTouchListener.java
+++ brainatlas-android/src/com/mm/brainatlas/listeners/MainOnTouchListener.java
@@ -2,6 +2,7 @@ package com.mm.brainatlas.listeners;
 
 import com.mm.brainatlas.activities.MainActivity;
 
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
@@ -10,6 +11,7 @@ public class MainOnTouchListener implements OnTouchListener {
 
 	private final MainActivity activity;
 	private float firstX = -1;
+	private boolean moved = false;
 	
 	
 	public MainOnTouchListener(MainActivity activity) {
@@ -20,10 +22,19 @@ public class MainOnTouchListener implements OnTouchListener {
 	public boolean onTouch(View arg0, MotionEvent arg1) {
 		if (arg1.getAction() == MotionEvent.ACTION_DOWN) {
 			firstX = arg1.getX();
+		} else if (arg1.getAction() == MotionEvent.ACTION_MOVE) {
+			if (arg1.getX() > firstX + 20 || arg1.getX() < firstX -20) {
+				moved = true;
+				Log.wtf("listener", "onMove");
+			}
 		} else if (arg1.getAction() == MotionEvent.ACTION_UP) {
-			if (arg1.getX() > firstX + 5 || arg1.getX() < firstX -5) 
+			if (arg1.getX() > firstX + 20 || arg1.getX() < firstX -20) 
 			firstX = -1;
-			activity.changeView();
+			boolean toMove = moved;
+			moved = false;
+			if (toMove) {
+				activity.changeView();
+			}
 		}		
 		return true;
 	}
